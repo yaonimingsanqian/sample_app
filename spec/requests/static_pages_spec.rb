75755a187a3bd88981b513d5b56ce416086df923
yaonimingsanqian <529670074@qq.com>
2013-08-13 09:49:34 +0800

    3章结束

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+
 describe "Home page" do
   it "should have the right title" do
     visit '/static_pages/home'
     expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
   end
-
-
 end
+
+describe "Help page" do
+  it "should have the right title" do
+    visit '/static_pages/help'
+    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+  end
+end
+
+describe "About page" do
+  it "should have the right title" do
+    visit '/static_pages/about'
+    expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+  end
+end
+
+describe "Contact page" do
+  it "should hanve the right contact" do
+    visit '/static_pages/contact'
+    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+  end
+end
+
 end
