2a5f1593894bc026f603795722c970f2fad546c2
yaonimingsanqian <529670074@qq.com>
2013-08-13 17:12:55 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,40 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-
-describe "Home page" do
-  it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
-  end
-end
-
-describe "Help page" do
-  it "should have the right title" do
-    visit '/static_pages/help'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-  end
-end
-
-describe "About page" do
-  it "should have the right title" do
-    visit '/static_pages/about'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
-  end
-end
-
-describe "Contact page" do
-  it "should hanve the right contact" do
-    visit '/static_pages/contact'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
-  end
-end
+  subject { page }
 
   describe "Home page" do
-    it "should have the |Home" do
-      visit '/static_pages/home'
-      expect(page).to have_title('| Home')
-    end
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "UserPages" do
+
+
+end
